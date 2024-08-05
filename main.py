caa7e6f801bed7e8dc96e86a5f8f9f93f0d8c629
bryce72 <brycedunlapsf56@gmail.com>
2024-08-04 22:55:22 -0400

    updated code: working on socket portion. Using DT

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
@@ -7,9 +7,53 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "secretKey123" ## TODO This should be encrypted!~~~~~~~~~
 socketio = SocketIO(app)
 
+rooms = {}
+
+def generate_unique_code(length):
+    while True:
+        code = ""
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
+            
+        if code not in rooms:
+            break
+    return code
+
 @app.route("/", methods=["POST", "GET"])
 def home():
+    session.clear()
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+        
+        if not name:
+            return render_template("home.html", error="ENTER A NAME!~", code=code, name=name)
+        
+        if join != False and not code:
+            return render_template("home.html", error="ENTER A ROOM CODE!~",code=code, name=name)
+        
+        room = code
+        if create != False:
+            room= generate_unique_code(4)
+            rooms[room] = {"members": 0, "messages": []}
+        elif code not in rooms:
+            return render_template("home.html", error="Room Doesnt exist!", code=code, name=name)
+
+        session["room"] = room
+        session["name"] = name
+        return redirect(url_for("room"))
+    
     return render_template("home.html")
+
+@app.route("/room")
+def room():
+    room = session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("home"))
+    
+    return render_template("room.html")
 
 if __name__ == "__main__":
     socketio.run(app,debug=True)
